cb570b689667be1f8180047a4e1a0d8b18c1ac5b
Aleenaas <aleenafrancis2510@gmail.com>
2024-12-17 16:58:23 +0530

    login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,49 +1,55 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Login.css';
-import { Link } from 'react-router-dom';
 
+const Login =()=>{
+    const registerLink=()=>{
+        setAction(' active');
+    }
+   
+    const universalStyles = {
+        fontFamily: 'Arial, sans-serif',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        
+      };
+    
+    return(
+        <div style={universalStyles}>
+        <div className='wrapper'>
+            <div className="form-box login">
+                <form action="">
+                    <h1>Login</h1>
+                    <div className='input-box'>
+                        <input type="text" placeholder='Username' required/>
+                    </div>
+                    {/* <div className='input-box'>
+                        <input type="email" placeholder='Email' required/>
+                    </div> */}
+                    
+                    <div className='input-box'>
+                        <input type="password" placeholder='Password' required/>
+                    </div>
+                    {/* <div className='input-box'>
+                        <input type="password" placeholder='ConfirmPassword' required/>
+                    </div> */}
+                    <div className='remember-forgot'>
+                        <a href="#">Forgot password?</a>
+                    </div>
 
-const Login = () => {
- 
+                    <button type='submit'>Login</button>
+                    <div className='register-link'>
+                        <p>Don't have an account?<a href='#' onClick={registerLink}> Register</a></p>
 
+                    </div>
 
-  return (
-    <div className="d-flex justify-content-center align-items-center min-vh-100 bg-light">
+                </form>
+            </div>
 
-      <div className="card p-4 shadow-lg" style={{ maxWidth: '400px', width: '100%' }}>
-        <form >
-          <h1 className="text-center text-success mb-4">Login</h1>
-          <div className="mb-3">
-            <label htmlFor="username" className="form-label">Username</label>
-            <input
-              type="text"
-              id="username"
-              className="form-control"
-             
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">Password</label>
-            <input
-              type="password"
-              id="password"
-              className="form-control"
-             
-              required
-            />
-          </div>
-          <button type="submit" className="btn btn-success w-100">Login</button>
-          <div className="text-center mt-3">
-            <p>
-              Don't have an account? <Link className='text-warning' to={'/registration'}>Register</Link>
-            </p>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-};
+        </div>
+        </div>
+    )
+}
+  
 
-export default Login;
+export default Login
